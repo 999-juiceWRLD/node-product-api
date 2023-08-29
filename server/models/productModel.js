87d928e6cac91443c30ada45c8b01ab907e2113d
juice wrld <ankaraunifedaisi@gmail.com>
2023-08-30 02:48:22 +0300

    create schema & model

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -1,1 +1,27 @@
 const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: ["true", "please enter the product name"]
+    },
+    quantity: {
+        type: Number,
+        default: 0,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: String,
+        required: false
+    }
+}, {
+    timestamps: true
+})
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;

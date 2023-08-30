2e03859779200a58fab8b499c50c5fe84c0df387
juice wrld <ankaraunifedaisi@gmail.com>
2023-08-30 21:08:18 +0300

    remove unnecessary imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Product } from './components/Product.component'
 import { Table } from './components/Table.component'
 
 /*
@@ -13,7 +12,6 @@ function App() {
     <div className='container my-3'>
       <h3 className='mb-4'>Product API</h3>
       <Table />
-      <Product />
     </div>
   )
 }

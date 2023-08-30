90225c3f1e6d17fff6ceb84ada50ac9571f6056a
juice wrld <ankaraunifedaisi@gmail.com>
2023-08-30 20:26:19 +0300

    include Table component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Product } from './components/Product.component'
+import { Table } from './components/Table.component'
 
 /*
 name, quantity, price, image (yok denecek kadar az)
@@ -11,6 +12,7 @@ function App() {
   return (
     <div className='container my-3'>
       <h3 className='mb-4'>Product API</h3>
+      <Table />
       <Product />
     </div>
   )

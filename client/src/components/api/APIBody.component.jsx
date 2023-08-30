e3c4c1f326d2646e34aeac98db1f029de0f1fb8d
juice wrld <ankaraunifedaisi@gmail.com>
2023-08-31 01:00:42 +0300

    remove unneccesary class names

--- client/src/components/api/APIBody.component.jsx
+++ client/src/components/api/APIBody.component.jsx
@@ -2,7 +2,7 @@ import { Table } from "./Table.component";
 
 export const APIBody = () => {
     return (
-      <div className='container my-3'>
+      <div>
         <h3 className='mb-4'>Product API</h3>
         <Table />
       </div>   

d12f1a178859893fbae90aeb8f39986f6ca752ba
juice wrld <ankaraunifedaisi@gmail.com>
2023-08-31 00:57:02 +0300

    use link component for routers

--- client/src/components/NavBar.component.jsx
+++ client/src/components/NavBar.component.jsx
@@ -1,18 +1,30 @@
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+
 export const NavBar = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="#">Home</a>
-                {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button> */}
-                <div className="" id="">
-                    <div className="navbar-nav flex flex-row">
-                        <a className="nav-link active px-3" aria-current="page" href="#">About</a>
-                        <a className="nav-link active px-3" href="#">Features</a>
+        <React.Fragment>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <Link className="text-decoration-none" to={'/home'}>
+                        <a className="navbar-brand">Home</a>
+                    </Link>
+                    <div className="" id="">
+                        <div className="navbar-nav flex flex-row">
+                            <Link className="text-decoration-none" to={'/api'}>
+                                <a className="nav-link active px-3">API</a>
+                            </Link>
+                            <Link className="text-decoration-none" to={'/about'}>
+                                <a className="nav-link active px-3">About</a>
+                            </Link>
+                            <Link className="text-decoration-none" to={'/feature'}>
+                                <a className="nav-link active px-3" href="#">Features</a>
+                            </Link>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+            <Outlet />
+        </React.Fragment>
     );
 }

9682d9f10b6af55f5a318dba551fe08c1200f8ae
juice wrld <ankaraunifedaisi@gmail.com>
2023-08-30 03:53:54 +0300

    create `getProduct()` method

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -5,8 +5,6 @@ const app = express();
 app.use(express.json());
 
 exports.postProduct = async (req, res) => {
-    console.log("post product is called");
-    console.log(req.body)
     try {
         const product = await Product.create(req.body)
         res.status(200).json({
@@ -15,6 +13,32 @@ exports.postProduct = async (req, res) => {
         })
     } catch (err) {
         console.log(err.message);
-        res.status(404);
+        res.status(404).json({ message: err.message });
+    }
+}
+
+exports.getProduct = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        res.status(200).json({
+            message: "success",
+            data: product
+        })
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).json({ message: err.message });
+    }
+}
+
+exports.getAllProducts = async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json({
+            message: "all the data is listed below.",
+            data: products
+        })
+    } catch (err) {
+        console.log(err.message);
+        res.status(404).json({ message: err.message });
     }
 }

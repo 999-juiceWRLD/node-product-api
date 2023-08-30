b5456a4627207bc79ec72f60f374fda54f7abed7
juice wrld <ankaraunifedaisi@gmail.com>
2023-08-30 04:10:21 +0300

    add `updateProduct()` method, and a new middleware for forms

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -3,6 +3,7 @@ const Product = require('../models/productModel');
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 exports.postProduct = async (req, res) => {
     try {
@@ -37,6 +38,22 @@ exports.getAllProducts = async (req, res) => {
             message: "all the data is listed below.",
             data: products
         })
+    } catch (err) {
+        console.log(err.message);
+        res.status(404).json({ message: err.message });
+    }
+}
+
+exports.updateProduct = async (req, res) => {
+    try {
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body);
+        if (!product) {
+            res.status(404).json({
+                message: "there's no such element. try with another id."
+            })
+        } else {
+            res.status(200).json({ message: "it's ok" });
+        }
     } catch (err) {
         console.log(err.message);
         res.status(404).json({ message: err.message });

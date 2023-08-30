d44293e7d226adf32ac9fb790f7f08308a6c3f18
juice wrld <ankaraunifedaisi@gmail.com>
2023-08-31 00:54:02 +0300

    add new wrapper for data table

---
+++ client/src/components/api/APIBody.component.jsx
@@ -0,0 +1,10 @@
+import { Table } from "./Table.component";
+
+export const APIBody = () => {
+    return (
+      <div className='container my-3'>
+        <h3 className='mb-4'>Product API</h3>
+        <Table />
+      </div>   
+    );
+}

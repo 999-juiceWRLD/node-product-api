6d1f7c048ea74c9c83c58380abde6b843d0f8182
juice wrld <ankaraunifedaisi@gmail.com>
2023-08-30 03:25:30 +0300

    implement post request, creates a new product

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -3,3 +3,18 @@ const Product = require('../models/productModel');
 
 const app = express();
 app.use(express.json());
+
+exports.postProduct = async (req, res) => {
+    console.log("post product is called");
+    console.log(req.body)
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json({
+            message: "it's added to db successfully.",
+            content: req.body
+        })
+    } catch (err) {
+        console.log(err.message);
+        res.status(404);
+    }
+}

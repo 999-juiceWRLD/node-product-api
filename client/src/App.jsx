5d06c3a129215d34bfbea2cc4e2b568bdbd73dda
juice wrld <ankaraunifedaisi@gmail.com>
2023-08-30 04:44:45 +0300

    start to experimental design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Product } from './components/Product.component'
+
+/*
+name, quantity, price, image (yok denecek kadar az)
+*/
 
 function App() {
 
   return (
     <div className='container my-3'>
-      <h1>What&apos;s up?</h1>
+      <h3 className='mb-4'>Product API</h3>
+      <Product />
     </div>
   )
 }
---
+++ client/src/components/Product.component.jsx
@@ -0,0 +1,9 @@
+export const Product = () => {
+    return (
+        <div className="row text-center pt-2">
+            <div className="col">name</div>
+            <div className="col">quantity</div>
+            <div className="col">price</div>
+        </div>
+    );
+}

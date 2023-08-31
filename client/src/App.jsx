41d95ef47d714e237f210ad007b24288bb8f60a2
juice wrld <ankaraunifedaisi@gmail.com>
2023-08-31 22:56:17 +0300

    change imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { NavBar } from './components/navbar/NavBar.component'
-import { About } from './components/about/About.component'
+import { Create } from './components/create/Create.component'
 import { APIBody } from './components/api/APIBody.component'
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route} from "react-router-dom"
 import { Home } from './components/home/Home.component'
+import { EditForm } from './components/edit/EditForm.component'
 
 function App() {
 
@@ -13,7 +14,8 @@ function App() {
       <Route path="/" element={<NavBar />}>
         <Route index={true} path='home' element={<Home />}/>
         <Route path="api" element={<APIBody />} />
-        <Route path="about" element={<About />} />
+        <Route path="api/edit/:id" element={<EditForm />} />
+        <Route path="api/product" element={<Create />} />
         <Route path="feature" element={<div> <h1>features</h1> </div>} />
       </Route>
     </Routes>

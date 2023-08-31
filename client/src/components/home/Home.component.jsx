7ca376a3a37a9d51c660a2c460206eb6f4d5c219
juice wrld <ankaraunifedaisi@gmail.com>
2023-08-31 23:13:38 +0300

    update homepage, add content

--- client/src/components/home/Home.component.jsx
+++ client/src/components/home/Home.component.jsx
@@ -1,7 +1,15 @@
 export const Home = () => {
     return (
         <div>
-            <h1>home</h1>
+            <h1 className="pb-3">welcome 🖐️</h1>
+            <p>I did this website, you can refer to my <a rel="noreferrer" href="https://github.com/999-juiceWRLD" target="_blank">github</a> account.</p>
+            <div className="my-2">
+                <p>
+                    This website is a simple representation of a product API, where you can make CRUD operations (create, read, update and delete records so-called products).
+                    As for backend, I used <a rel="noreferrer" href="https://nodejs.org/en" target="_blank">Node.js</a>. On the frontend, <a rel="noreferrer" href="https://react.dev/">React.js</a>.
+                </p>
+                <p>If you want to see the source code, refer <a rel="noreferrer" href="https://github.com/999-juiceWRLD/node-product-api" target="_blank">here.</a></p>
+            </div>
         </div>
     );
 }

6efd3f66ad6cde004b4f7dcce28565c4e404eca6
juice wrld <ankaraunifedaisi@gmail.com>
2023-08-31 23:03:09 +0300

    make dynamic url for each element's id

--- client/src/components/api/Body.component.jsx
+++ client/src/components/api/Body.component.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios"
+import { Link } from "react-router-dom";
 
 export const Body = () => {
 
@@ -33,7 +34,7 @@ export const Body = () => {
                     <td>{e.quantity}</td>
                     <td>{e.price}$</td>
                     <td><button type="button" style={{ fontSize: "0.7rem" }} className="btn btn-danger" onClick={() => deleteRequest(e)}>Delete</button></td>
-                    <td><button type="button" style={{ fontSize: "0.7rem" }} className="btn btn-warning">Edit</button></td>
+                    <td><Link to={`edit/${e._id}`}><button type="button" style={{ fontSize: "0.7rem" }} className="btn btn-warning">Edit</button></Link></td>
                 </tr>
             ))}
         </tbody>

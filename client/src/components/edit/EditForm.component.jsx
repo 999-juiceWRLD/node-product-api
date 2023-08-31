ccf81443b6077c56b1a88f53a09a4bc6279825f8
juice wrld <ankaraunifedaisi@gmail.com>
2023-09-01 02:37:46 +0300

    edit form component is done

--- client/src/components/edit/EditForm.component.jsx
+++ client/src/components/edit/EditForm.component.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
-import PropTypes from "prop-types"
+import { StatusComp } from "../create/StatusComp.component"
 import axios from "axios"
 
 export const EditForm = () => {
     
     const [values, setValues] = useState({ name: '', quantity: '', price: '' })
+    const [isSuccess, setSuccess] = useState(false);
     const { id } = useParams();
 
     const url = 'http://localhost:3000/api';
@@ -24,6 +25,16 @@ export const EditForm = () => {
             })
             .catch(err => { console.log(err) })
     }, [id]);
+
+    const updateData = async (e) => {
+        e.preventDefault();
+        await axios.patch(`${url}/product/${id}`, values)
+            .then(message => { 
+                if (message.status === 200) {
+                    setSuccess(prev => ( !prev ))
+                }})
+            .catch(err => { console.log(err) })
+    }
     const setParam = (name) => {
         // console.log(name);
         return ({ target: { value } }) => {
@@ -34,7 +45,8 @@ export const EditForm = () => {
     return (
         <div>
             <h1 className="my-3">Edit record</h1>
-            <form className="mt-2" action="PATCH">
+            {isSuccess ? (<StatusComp />) : null }
+            <form className="mt-2" action="PATCH" onSubmit={updateData}>
                 <div className="mb-3">
                 <input className="form-control" id="disabledInput" type="text" placeholder={`id: ${id}`} disabled />
                 </div>
@@ -43,13 +55,21 @@ export const EditForm = () => {
                     <input type="text" value={values.name} required className="form-control" id="name"
                            onChange={setParam('name')}/>
                 </div>
+                <div className="mb-3">
+                    <label className="form-label">Quantity</label>
+                    <input type="number" value={values.quantity} required className="form-control" id="quantity" 
+                           onChange={setParam('quantity')}/>
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Price</label>
+                    <input type="number" value={values.price} required className="form-control" id="price" 
+                           onChange={setParam('price')}/>
+                </div>
+                <div className="d-flex justify-content-end my-4">
+                    <button type="submit" className="btn btn-primary px-5">Save</button>
+                </div>
             </form>
         </div>
     );
 }
 
-EditForm.propTypes = {
-    name: PropTypes.string,
-    quantity: PropTypes.number,
-    price: PropTypes.number
-}
